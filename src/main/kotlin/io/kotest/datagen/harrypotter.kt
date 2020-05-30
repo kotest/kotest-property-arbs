0e4b7ead741be65995a515250552f26961e9e4a1
sksamuel <sam@sksamuel.com>
2020-05-30 13:00:22 -0500

    Fixed csv for harry last names

--- src/main/kotlin/io/kotest/datagen/harrypotter.kt
+++ src/main/kotlin/io/kotest/datagen/harrypotter.kt
@@ -5,3 +5,7 @@ object HarryPotterCharacterProducer : Producer<Name> {
    private val last = loadResourceAsLines("/harrypotter_last_names.csv")
    override fun produce(): Name = Name(FirstName(first.random()), LastName(last.random()))
 }
+
+fun main() {
+   HarryPotterCharacterProducer.take(100).forEach(::println)
+}
