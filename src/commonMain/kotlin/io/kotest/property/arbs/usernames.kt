23fc381e7c782ce9300aa597882b2ae6dd20e727
sksamuel <sam@sksamuel.com>
2021-12-23 07:35:54 -0600

    Added usernames arb

---
+++ src/commonMain/kotlin/io/kotest/property/arbs/usernames.kt
@@ -0,0 +1,96 @@
+package io.kotest.property.arbs
+
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.arbitrary
+
+data class Username(val value: String)
+
+private val colours = listOf(
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "cyan",
+  "blue",
+  "magenta",
+  "purple",
+  "white",
+  "black",
+  "grey",
+  "silver",
+  "pink",
+  "maroon",
+  "brown",
+  "beige",
+  "tan",
+  "peach",
+  "lime",
+  "olive",
+  "turquoise",
+  "teal",
+  "navy blue",
+  "indigo",
+  "violet",
+)
+
+private val sizes = listOf(
+  "large",
+  "small",
+  "medium",
+  "giant",
+  "huge",
+  "tiny",
+  "minor",
+  "major",
+  "big",
+  "little",
+  "mini",
+  "maxi",
+  "pocket",
+  "micro",
+  "slight",
+  "great",
+  "massive",
+  "colossal",
+  "broad",
+)
+
+private val animals = listOf(
+  "hog",
+  "pig",
+  "cow",
+  "dog",
+  "cat",
+  "mouse",
+  "rat",
+  "rabbit",
+  "sheep",
+  "horse",
+  "donkey",
+  "ass",
+  "monkey",
+  "chimp",
+  "fox",
+  "hare",
+  "goose",
+  "duck",
+  "frog",
+  "toad",
+  "beetle",
+  "hound",
+  "elephant",
+  "lion",
+  "tiger",
+  "cougar",
+  "panther",
+  "leopard"
+)
+
+fun Arb.Companion.usernames() = arbitrary { rs ->
+  listOf(
+    sizes.random(rs.random),
+    colours.random(rs.random),
+    animals.random(rs.random),
+    rs.random.nextInt(1000, 9999)
+  ).joinToString("_")
+}
--- src/jvmMain/kotlin/io/kotest/property/arbs/run.kt
+++ src/jvmMain/kotlin/io/kotest/property/arbs/run.kt
@@ -2,8 +2,7 @@ package io.kotest.property.arbs
 
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.take
-import io.kotest.property.arbs.payments.transactions
 
 fun main() {
-  Arb.transactions().take(100).forEach(::println)
+  Arb.usernames().take(100).forEach(::println)
 }
