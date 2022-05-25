704327f401ba1cd55552a6ef323007c9c19ef517
Sam Sam <sam@sksamuel.com>
2022-05-24 23:34:22 -0500

    Update Libs.kt

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,17 +1,17 @@
 object Libs {
 
-  const val kotlinVersion = "1.6.10"
+  const val kotlinVersion = "1.6.21"
   const val org = "io.kotest.extensions"
   const val stdLib = "org.jetbrains.kotlin:kotlin-stdlib-common:$kotlinVersion"
 
   object Kotlin {
-    private const val version = "1.6.0"
+    private const val version = "1.6.21"
     const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
     const val datetime = ""
   }
 
   object Kotest {
-    private const val version = "5.0.3"
+    private const val version = "5.3.0"
     const val property = "io.kotest:kotest-property:$version"
     const val propertyDateTime = "io.kotest.extensions:kotest-property-datetime:1.1.0"
     const val api = "io.kotest:kotest-framework-api:$version"
