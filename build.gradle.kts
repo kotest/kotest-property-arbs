ec2a1266b4ee9cdb556262bbee10b7e90cbd1ad4
sksamuel <sam@sksamuel.com>
2026-05-07 11:05:43 -0500

    Removed obsolete Node.js configuration related to resolved Kotlin issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -124,8 +124,3 @@ version = Ci.version
 //    }
 //  }
 //}
-
-// TODO: Remove me after https://youtrack.jetbrains.com/issue/KT-49109 is fixed
-rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
-  rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().nodeVersion = "16.0.0"
-}
