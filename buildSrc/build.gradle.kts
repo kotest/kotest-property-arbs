79ec71b19fae1b79bb87d59ca85e227bf6eafa65
sksamuel <sam@sksamuel.com>
2026-05-07 20:36:40 -0500

    Remove redundant Maven Central repository declaration in buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,10 +2,6 @@ plugins {
   `kotlin-dsl`
 }
 
-repositories {
-  mavenCentral()
-}
-
 dependencies {
   implementation("com.univocity:univocity-parsers:2.9.1")
   testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
